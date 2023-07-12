4ebd95608b3947ebacc11aeaa689ba0dbe4ea8fe
SejalJagtap <sejaljagtap@gmail.com>
2023-07-12 22:33:27 +0530

    added

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 
+
 const Login = () => {
 	const [data, setData] = useState({ email: "", password: "" });
 	const [error, setError] = useState("");
@@ -30,6 +31,7 @@ const Login = () => {
 	};
 
 	return (
+	  <div className={styles.bg }>
 		<div className={styles.login_container}>
 			<div className={styles.login_form_container}>
 				<div className={styles.left}>
@@ -55,7 +57,7 @@ const Login = () => {
 						/>
 						{error && <div className={styles.error_msg}>{error}</div>}
 						<button type="submit" className={styles.green_btn}>
-							Sing In
+							Sign In
 						</button>
 					</form>
 				</div>
@@ -63,11 +65,12 @@ const Login = () => {
 					<h1>New Here ?</h1>
 					<Link to="/signup">
 						<button type="button" className={styles.white_btn}>
-							Sing Up
+							Sign Up
 						</button>
 					</Link>
 				</div>
 			</div>
+		</div>
 		</div>
 	);
 };
